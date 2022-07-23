4fa292a8ae8039d55741473467592148585b9864
Ben <srfeesha@gmail.com>
2022-07-23 19:12:42 +0200

    style(button): added interactivity

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -1,7 +1,17 @@
-export const Button = ({ children }) => {
+import clsx from "clsx"
+
+export const Button = ({ icon, className, children }) => {
   return (
-    <button className="px-4 py-2 text-lg bg-blue-500 border border-blue-800 rounded-xl min-w-min">
-      {children}
+    <button
+      className={clsx(
+        className,
+        "px-4 py-2 text-lg text-white bg-blue-500 border-[3] border-blue-600 rounded-xl w-fit drop-shadow-sm hover:drop-shadow-lg ease-in-out active:bg-blue-600 hover:border-blue-700 duration-300"
+      )}
+    >
+      <span className="flex items-center gap-2 ">
+        {icon && icon}
+        {children}
+      </span>
     </button>
-  );
-};
+  )
+}
