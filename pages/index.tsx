fb0052bd7b4d876381073810e1f3ea7b5632905a
Ben <srfeesha@gmail.com>
2022-07-23 19:54:56 +0200

    feat(button): added linkedin link

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
+import link from 'next/link'
 import Image from 'next/image'
 import { Button } from '../src/components/button'
 import { Linkedin } from "../src/components/icons"
@@ -14,7 +14,11 @@ const Home: NextPage = () => {
           <div className='flex flex-col justify-center gap-6 lg:basis-3/4'>
             <h1 className='text-4xl text-center lg:leading-tight lg:text-5xl text-zinc-800 md:text-start'>I’m Beniamino Marini, Product Designer with 3+ years of experience. </h1>
             <p className='text-xl text-center md:text-start text-zinc-500'>Open to new opportunities · Currently designing at Wonderflow</p>
-            <Button icon={<Linkedin className="w-4 h-4 fill-white" />} className="self-center md:self-start" >Connect on Linkedin</Button>
+            <Button icon={<Linkedin className="w-4 h-4 fill-white" />} className="self-center md:self-start" >
+              <a href="https://www.linkedin.com/in/youcancallmeben/" target="_blank" rel='noopener noreferrer'>
+                Connect on Linkedin
+              </a>
+            </Button>
           </div>
           <Image src={BenCircle} alt='Beniamino Marini Product Designer' className='rounded-full sm:basis-1/4' />
         </CrazyCard>
