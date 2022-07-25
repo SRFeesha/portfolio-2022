254a39525451d1f100997a9574f31a806b0d1bf5
Ben <srfeesha@gmail.com>
2022-07-25 16:45:48 +0200

    feat(button): added 3 different button kind

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -1,11 +1,20 @@
 import clsx from "clsx"
 
-export const Button = ({ icon, className, children }) => {
+export const Button = ({ kind = "primary", icon, className, children }) => {
+  const hierarchy = {
+    primary:
+      "text-white bg-blue-500 border-blue-600 active:bg-blue-600 hover:border-blue-700 hover:drop-shadow-lg ",
+    secondary:
+      "bg-white hover:border-slate-300 hover:drop-shadow active:bg-slate-200 ",
+    flat: "drop-shadow-none border-none hover:bg-slate-200 active:bg-slate-300 ease-in",
+  }
+
   return (
     <button
       className={clsx(
-        className,
-        "px-4 py-2 text-lg text-white bg-blue-500 border-[3] border-blue-600 rounded-xl w-fit drop-shadow-sm hover:drop-shadow-lg ease-in-out active:bg-blue-600 hover:border-blue-700 duration-300"
+        "px-4 py-2 text-lg border-2 rounded-xl w-fit drop-shadow-sm  ease-in-out duration-300 active:drop-shadow-none",
+        hierarchy[kind],
+        className
       )}
     >
       <span className="flex items-center gap-2 ">
