f3cc26e1e6025676d266cf1a4fe071da556e01c3
Ben <srfeesha@gmail.com>
2022-07-25 15:15:32 +0200

    style(crazy-card): added fake border gradient

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,18 @@
-import type { NextPage } from 'next'
-import link from 'next/link'
 import Image from 'next/image'
 import { Button } from '../src/components/button'
 import { Linkedin } from "../src/components/icons"
 import BenCircle from '../public/assets/img/Beniamino-Marini--Squared--400x400.png'
 import { CrazyCard } from '../src/components/crazy-card'
 
-const Home: NextPage = () => {
+const Home = () => {
   return (
     <main className='flex items-center justify-center min-h-screen bg-gray-100 lg:overflow-hidden lg:h-screen '>
       <div className='container flex justify-center px-2 py-8 mx-auto align-middle md:py-64 xl:px-48'>
         <CrazyCard >
-          <div className='flex flex-col justify-center gap-6 lg:basis-3/4'>
+          <div className='flex flex-col justify-center gap-4 lg:basis-3/4'>
             <h1 className='text-4xl text-center lg:leading-tight lg:text-5xl text-zinc-800 md:text-start'>I’m Beniamino Marini, Product Designer with 3+ years of experience. </h1>
-            <p className='text-xl text-center md:text-start text-zinc-500'>Open to new opportunities · Currently designing at Wonderflow</p>
-            <Button icon={<Linkedin className="w-4 h-4 fill-white" />} className="self-center md:self-start" >
+            <p className='text-lg text-center md:text-xl md:text-start text-zinc-500'>Open to new opportunities · Currently designing at Wonderflow</p>
+            <Button icon={<Linkedin className="w-4 h-4 fill-white" />} className="self-center mt-4 md:self-start" >
               <a href="https://www.linkedin.com/in/youcancallmeben/" target="_blank" rel='noopener noreferrer'>
                 Connect on Linkedin
               </a>
