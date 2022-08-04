689b332c9ed92e4176cc534b16832b3fa0d74605
Ben <srfeesha@gmail.com>
2022-08-04 14:28:30 +0200

    feat(catalog-configurator): init files

---
+++ pages/case-studies/catalog-configurator.jsx
@@ -0,0 +1,7 @@
+import Image from "next/image"
+
+const CatalogConfigurator = () => {
+  return <h1>Catalog Configurator</h1>
+}
+
+export default CatalogConfigurator
---
+++ pages/case-studies/index.jsx
@@ -0,0 +1,10 @@
+const CaseStudies = () => (
+  <main className="container mx-auto">
+    <article className="p-16 radius-lg bg-stone-100 border-stone-200 rounded-xl">
+      <h2>Wonderflow</h2>
+      <a href="./catalog-configurator.jsx">Catalog configurator</a>
+    </article>
+  </main>
+)
+
+export default CaseStudies
