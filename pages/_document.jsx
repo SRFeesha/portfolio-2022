3302e82bb713c3dde470d1246af79f9e13bdbaa0
Ben <srfeesha@gmail.com>
2022-08-09 15:11:24 +0200

    feat(seo): added favicon

---
+++ pages/_document.jsx
@@ -0,0 +1,21 @@
+import { Html, Head, Main, NextScript } from "next/document"
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>
+          ✨
+          </text></svg>"
+        />
+        <title>Beniamino Marini — Product Designer</title>
+      </Head>
+      <body className="text-lg bg-stone-50">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
