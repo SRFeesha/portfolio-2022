cb63fd1ce0bc9d5dc5c351e2e93be5432f2d22d8
Ben <srfeesha@gmail.com>
2022-10-09 20:24:23 +0200

    style(explicative-picture): adjusted h-alignment

--- src/components/explicative-picture/explicative-picture.jsx
+++ src/components/explicative-picture/explicative-picture.jsx
@@ -8,14 +8,14 @@ export const ExplicativePicture = ({ src, caption, className, alt }) => {
     <figure className="mt-10 -mx-3 mb-14">
       <div
         className={clsx(
-          "relative p-2 mb-3 xl:p-3 md:-mx-0 xl:-mx-8 md:rounded-lg border",
+          "relative p-2 mb-3 xl:p-3 md:mx-0 md:rounded-lg ",
           className
         )}
       >
         <Zoom>
           <Image src={src} style={{ borderRadius: "4px" }} alt={alt} />
         </Zoom>
-        <figcaption className="mx-2 font-light leading-snug md:ml-1 text-stone-500">
+        <figcaption className="mx-2 font-serif leading-snug md:ml-1 text-stone-500">
           {caption}
         </figcaption>
       </div>
