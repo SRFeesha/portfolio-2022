dfd7230929cecabf64d66ab33e7bffc8f114d9b9
Ben <srfeesha@gmail.com>
2022-10-30 13:33:33 +0100

    feat(analytics): added splitbee, removed hotjar

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,20 +1,17 @@
 import "../styles/globals.css"
 import "@/assets/font/sligoil-micro/style.css"
+import Script from "next/script"
 import { Navigation } from "@/components/navigation"
 import { Footer } from "@/components/footer"
-import { hotjar } from "react-hotjar"
 import { useEffect } from "react"
 
 const MyApp = ({ Component, pageProps }) => {
-  useEffect(() => {
-    hotjar.initialize(3171308, 6)
-  }, [])
-
   return (
     <>
       <Navigation />
       <Component {...pageProps} />
       <Footer />
+      <Script src="https://cdn.splitbee.io/sb.js" />
     </>
   )
 }
